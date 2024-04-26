89c4b296709980e506bd9a1ae87c6abae444d347
Mvson2 <62848351+Mvson2@users.noreply.github.com>
2024-04-26 19:20:07 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -6,23 +6,3 @@ function showMenu() {
       x.style.display = "none";
     }
     }
-    function showDetails() {
-        var x = document.getElementById("profile-toggle1");
-        if (x.style.display === "none") {
-          x.style.display = "block";
-        } else {
-          x.style.display = "none";
-        }
-        var x = document.getElementById("profile-toggle2");
-        if (x.style.display === "none") {
-          x.style.display = "block";
-        } else {
-          x.style.display = "none";
-        }
-                var x = document.getElementById("profile-toggle3");
-        if (x.style.display === "none") {
-          x.style.display = "block";
-        } else {
-          x.style.display = "none";
-        }
-        }
